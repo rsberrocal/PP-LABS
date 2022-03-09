d7fbbb9819b689d2f29a3b08533f86f6f0c52522
angelass000 <angsersan@gmail.com>
2022-03-09 18:27:47 +0100

    Avanzamiento
    Avanzado

--- lab1_submit/main.cpp
+++ lab1_submit/main.cpp
@@ -40,6 +40,67 @@ bool checkResults(uchar4* rgba, uchar3* grb, int size) {
     return correct;
 }
 
+// Function optimized with 2 loops paralleled by rows
+void convertGRB2RGBA3_rows(uchar3* grb, uchar4* rgba, int width, int height) {
+	#pragma omp parallel 
+	{
+	for (int x=0; x<height; ++x) {
+		#pragma omp for
+		for (int y=0; y<width; ++y) {	
+			rgba[height * y + x].x = grb[height * y + x].y;
+			rgba[height * y + x].y = grb[height * y + x].x;
+			rgba[height * y + x].z = grb[height * y + x].z;
+			rgba[height * y + x].w = 255;
+		}
+	}
+	}              
+}
+
+// Function optimized with 2 loops paralleled by columns
+void convertGRB2RGBA3_columns(uchar3* grb, uchar4* rgba, int width, int height) {
+	for (int x=0; x<height; ++x) {
+		#pragma omp parallel 
+		{
+		for (int y=0; y<width; ++y) {
+			#pragma omp single
+			{
+			rgba[height * y + x].x = grb[height * y + x].y;
+			rgba[height * y + x].y = grb[height * y + x].x;
+			rgba[height * y + x].z = grb[height * y + x].z;
+			rgba[height * y + x].w = 255;
+			}
+		}
+		}
+	}              
+}
+
+// Function optimized with 1 loop paralleled 
+void convertGRB2RGBA3(uchar3* grb, uchar4* rgba, int width, int height) {
+	#pragma omp parallel for
+    for(int x = 0; x < width * height; x++){
+        rgba[x].x = grb[x].y;
+        rgba[x].y = grb[x].x;
+        rgba[x].z = grb[x].z;
+        rgba[x].w = 255;
+    }    
+}
+
+
+// Function optimized by two loops
+void convertGRB2RGBA2_(uchar3* grb, uchar4* rgba, int width, int height) {
+	{
+	for (int x=0; x<height; ++x) {
+		for (int y=0; y<width; ++y) {	
+			rgba[height * y + x].x = grb[height * y + x].y;
+			rgba[height * y + x].y = grb[height * y + x].x;
+			rgba[height * y + x].z = grb[height * y + x].z;
+			rgba[height * y + x].w = 255;
+		}
+	}
+	}              
+}
+
+// Function optimized by one loops
 void convertGRB2RGBA2(uchar3* grb, uchar4* rgba, int width, int height) {
     for(int x = 0; x < width * height; x++){
         rgba[x].x = grb[x].y;
@@ -80,8 +141,9 @@ int main() {
 
     auto t1 = std::chrono::high_resolution_clock::now();
     for (int i=0; i<EXPERIMENT_ITERATIONS; ++i) {    
-        //convertGRB2RGBA(h_grb, h_rgba, WIDTH, HEIGHT);
-        convertGRB2RGBA2(h_grb, h_rgba, WIDTH, HEIGHT);
+		convertGRB2RGBA3_rows(h_grb, h_rgba, WIDTH, HEIGHT);
+		//convertGRB2RGBA3_columns(h_grb, h_rgba, WIDTH, HEIGHT);
+		//convertGRB2RGBA3(h_grb, h_rgba, WIDTH, HEIGHT);
     }
     auto t2 = std::chrono::high_resolution_clock::now();
 
